9b40ae220d5f879a611126e663c3dfac515e78cf
sergeyzoloto <sergiozoloto@gmail.com>
2023-04-16 23:28:52 +0200

    backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const User = require('./models/User');
-const { checkAuthenticated } = require('./controllers/authController');
+const { checkAuthenticated } = require('./controllers/authController.js');
 
 // Connect to MongoDB
-const connectDB = require('./config/db');
+const connectDB = require('./db.js');
 connectDB();
 
-const initializePassport = require('./passport-config');
+const initializePassport = require('./passport-config.js');
 initializePassport(
   passport,
   async (email) => await User.findOne({ email: email }),
@@ -33,7 +33,7 @@ app.use(passport.session());
 app.use(methodOverride('_method'));
 
 // Routes
-const authRouter = require('./routes/auth');
+const authRouter = require('./routes/auth.js');
 app.use(authRouter);
 
 app.get('/', checkAuthenticated, (req, res) => {
